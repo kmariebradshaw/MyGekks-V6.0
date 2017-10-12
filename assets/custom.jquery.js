59d49fcdb14fe3bedf03b39406828f31b84520f1
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2017-10-12 12:49:57 -0500

    finalizes exit offer trigger, once per session, not if other pop-ups are visible, on mouseout of window

--- assets/custom.jquery.js
+++ assets/custom.jquery.js
@@ -1,3 +1,47 @@
+    // $(function() {
+    //     // Cookies
+    //     function setCookie(name, days) {
+    //         if (days) {
+    //           document.
+    //             var date = new Date();
+    //             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    //             var expires = "; expires=" + date.toGMTString();
+    //         }
+    //         else var expires = "";
+
+    //         document.cookie = name + "=" + expires + "; path=/";
+    //     }
+
+    //     function getCookie(name) {
+    //         var nameEQ = name + "=";
+    //         var ca = document.cookie.split(';');
+    //         for (var i = 0; i < ca.length; i++) {
+    //             var c = ca[i];
+    //             while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    //             if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    //         }
+    //         return null;
+    //     }
+
+    //     var myCookie = getCookie("GekksVisit");
+    //     if (myCookie == null) {
+    //      $(document).on('mouseleave', leaveFromTop);
+    //        var executed = false;  
+    //        if (!executed) {
+    //         function leaveFromTop(e){
+    //           if( e.clientY < 0 ) {
+    //            $('#exit_offer').fadeIn();
+    //           }
+    //         }
+    //       }
+    //         setCookie("GekksVisit", 7);
+    //     }
+    //     else {
+    //         $('.mydiv').css('display','none');
+    //     }
+    // });
+
+
 if(window.location.href.indexOf("countdown") > -1) {
   localStorage.setItem("timer", true);
 }
