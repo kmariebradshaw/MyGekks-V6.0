71cd8d0b3f2ecbc37bd69fefaae8aa4c379e9487
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2017-12-08 12:00:20 -0600

    script for hiding expedited shipping on products we are out of, this is happening manually but has significant room for improvement

---
+++ assets/customcheckout.js
@@ -0,0 +1,16 @@
+function orderContains() {
+    var cart = $.ajax({type: "GET", url: "/cart.js", async: false}).responseText;
+    console.log(cart)
+    if (!(cart.indexOf('3161957') == -1) || !(cart.indexOf('82496') == -1) || !(cart.indexOf('182490') == -1))  {
+        $('[data-shipping-method="shopify-Same%20Day%20Shipping%20(Delivered%20in%202%20-%205%20Business%20Days)-7.90"]').parent().hide();
+        $('[data-shipping-method="shopify-Expedited%20Shipping%20(Delivered%20in%203%20Business%20Days)-14.90').parent().hide();
+    }
+}
+
+function timeOfOrder() {
+
+}
+
+$(document).ready(function() {
+    orderContains(); 
+}); 
